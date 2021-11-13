e4d415c373d6c31c5812b2bce8c862f31b4ec7af
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-13 05:31:41 +0200

    Components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div className="App">
+          <header className="App-header">
+       <p></p>
+          </header>
+        </div>
+      );
+}
+
+export default Header;
