3b561bb06e5bceddb8ea34f7f1d31df6980e74f9
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-16 02:57:44 +0200

    Create Preloader.js

---
+++ src/components/Preloader.js
@@ -0,0 +1,8 @@
+export default function Preloader() {
+    return [
+        
+            <div id="preloader"></div>,
+            <a href="#" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
+        
+    ];
+};
