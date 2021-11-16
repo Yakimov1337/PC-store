69b5afde99451e68c1dcdaf2f021c7920a3d911b
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-16 02:57:37 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,50 @@
+
+
 function Header() {
     return (
-        <div className="App">
-          <header className="App-header">
-       <p></p>
-          </header>
-        </div>
+      <header id="header" className="fixed-top ">
+      <div className="container d-flex align-items-center justify-content-lg-between">
+
+        <h1 className="logo me-auto me-lg-0"><a href="index.html">Pc store<span>.</span></a></h1>
+
+
+
+        <nav id="navbar" className="navbar order-last order-lg-0">
+          <ul>
+            <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
+            <li><a className="nav-link scrollto" href="#about">About</a></li>
+            <li><a className="nav-link scrollto" href="#services">Services</a></li>
+            <li><a className="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+            <li><a className="nav-link scrollto" href="#team">Team</a></li>
+            <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
+              <ul>
+                <li><a href="#">Drop Down 1</a></li>
+                <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
+                  <ul>
+                    <li><a href="#">Deep Drop Down 1</a></li>
+                    <li><a href="#">Deep Drop Down 2</a></li>
+                    <li><a href="#">Deep Drop Down 3</a></li>
+                    <li><a href="#">Deep Drop Down 4</a></li>
+                    <li><a href="#">Deep Drop Down 5</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Drop Down 2</a></li>
+                <li><a href="#">Drop Down 3</a></li>
+                <li><a href="#">Drop Down 4</a></li>
+              </ul>
+            </li>
+            <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
+          </ul>
+          <i className="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+
+        <a href="#about" className="get-started-btn scrollto">Login</a>
+        <a href="#about" className="get-started-btn scrollto">Sign Up!</a>
+
+
+      </div>
+    </header>
+
       );
 }
 
