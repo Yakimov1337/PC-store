6f2692af12e7321e2e1c2c40aab5936fc0329827
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-22 05:46:52 +0200

    Main fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,53 @@ export default function Main() {
 
     return (
         <div>
+            <div>
+                <section id="hero" className="d-flex align-items-center justify-content-center">
+                    <div className="container" data-aos="fade-up">
+
+                        <div className="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+                            <div className="col-xl-6 col-lg-8">
+                                <h1>Powerful Digital Solutions By PC Store<span>.</span></h1>
+                                <h2>We are team of talented digital marketers</h2>
+                            </div>
+                        </div>
+
+                        <div className="row gy-4 mt-5 justify-content-center" data-aos="zoom-in" data-aos-delay="250">
+                            <div className="col-xl-2 col-md-4">
+                                <div className="icon-box">
+                                    <i class='bx bx-store'></i>
+                                    <h3><a href="">Biggest Marketplace</a></h3>
+                                </div>
+                            </div>
+                            <div className="col-xl-2 col-md-4">
+                                <div className="icon-box">
+                                    <i className="ri-bar-chart-box-line"></i>
+                                    <h3><a href="">Financial Service</a></h3>
+                                </div>
+                            </div>
+                            <div className="col-xl-2 col-md-4">
+                                <div className="icon-box">
+                                    <i className="ri-calendar-todo-line"></i>
+                                    <h3><a href="">Analytics</a></h3>
+                                </div>
+                            </div>
+                            <div className="col-xl-2 col-md-4">
+                                <div className="icon-box">
+                                    <i className="ri-paint-brush-line"></i>
+                                    <h3><a href="">Repair Service</a></h3>
+                                </div>
+                            </div>
+                            <div className="col-xl-2 col-md-4">
+                                <div className="icon-box">
+                                    <i className="ri-database-2-line"></i>
+                                    <h3><a href="">Storage</a></h3>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+            </div>
             <main id="main">
                 <section id="about" className="about">
                     <div className="container" data-aos="fade-up">
@@ -69,7 +116,7 @@ export default function Main() {
                                 <div className="icon-box">
                                     <div className="icon"><i class='bx bx-store'></i></div>
                                     <h4><a href="">Marketplace</a></h4>
-                                    <p>Buy or sell new and used items easily on PC Store Marketplace, locally or from businesses. Find great deals on new items shipped from stores to your door.</p>
+                                    <p>PC Store Marketplace makes it simple to buy and sell new and used things locally or from businesses. Find great prices on new things delivered straight to your home from retailers.</p>
                                 </div>
                             </div>
 
@@ -120,7 +167,7 @@ export default function Main() {
                     </div>
                 </section>
 
-                
+
                 <section id="features" className="features">
                     <div className="container" data-aos="fade-up">
                         <div className="section-title">
@@ -163,9 +210,9 @@ export default function Main() {
                         <div className="text-center">
                             <h3>Get Started</h3>
                             <p> Explore our biggest and trusted marketplace.Find solution for your business.Satisfy your needs.</p>
-                          
+
                             <a className="cta-btn" href="#">Sign Up</a>
-                            
+
                         </div>
 
                     </div>
