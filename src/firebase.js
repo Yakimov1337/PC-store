0fef635fa1b263d4cac2d043cb06fe5cb25be069
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-23 07:32:10 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/app";
+import firebase from "firebase/compat/app";
 import "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,7 +11,7 @@ const app = firebase.initializeApp ({
   projectId: process.env.REACT_APP_FIREBASE_API_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_API_PROJECT_APP_STORAGE_BUCKET ,
   messagingSenderId: process.env.REACT_APP_FIREBASE_PROJECT_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_API_PROJECT_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
 // Initialize Firebase
