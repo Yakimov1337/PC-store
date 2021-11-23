62bb223e23217dbd433ca63ab1d8ddcc100840d3
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-23 05:27:36 +0200

    Auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import firebase from "firebase/app";
+import "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBVBt0JTTo77nw_uZGITSI9lEyA4tD2vqk",
-  authDomain: "pc-store-85d68.firebaseapp.com",
-  projectId: "pc-store-85d68",
-  storageBucket: "pc-store-85d68.appspot.com",
-  messagingSenderId: "223345808053",
-  appId: "1:223345808053:web:d0a5d13c1600ca6c78e4fc"
-};
+const app = firebase.initializeApp ({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_API_AUTH_DOMAIN ,
+  projectId: process.env.REACT_APP_FIREBASE_API_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_API_PROJECT_APP_STORAGE_BUCKET ,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_PROJECT_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_API_PROJECT_APP_ID
+});
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const auth = app.auth();
+export default app;
