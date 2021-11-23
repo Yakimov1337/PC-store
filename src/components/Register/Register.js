63d9722c51164effbde912846873cf9b2d31dfc6
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-23 07:32:22 +0200

    Register auth

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,34 @@
+import React, { useRef, useState } from 'react';
+import { useAuth } from '../../contexts/AuthContext';
+
 export default function Register() {
     require('./css/style.css');
+    
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
+    const { signup, currentUser } = useAuth();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError('Password do not match!')
+        }
+
+        try {
+            setError('');
+            setLoading(true);
+            await signup(emailRef.current.value, passwordRef.current.value);
+
+        }catch{
+            setError('Failed to create an account!');
+        }
+        setLoading(false);
+    }
+
     return (
         <div>
             <meta charSet="UTF-8" />
@@ -16,20 +45,20 @@ export default function Register() {
                     {/* <img src="images/signup-bg.jpg" alt=""> */}
                     <div className="container-reg">
                         <div className="signup-content">
-                            <form method="POST" id="signup-form" className="signup-form">
+                            <form method="POST" onSubmit= {handleSubmit} id="signup-form" className="signup-form">
                                 <h2 className="form-title">Create account</h2>
-                                <div className="form-group">
+                                {/* <div className="form-group">
                                     <input type="text" className="form-input" name="name" id="name" placeholder="Your Name" />
+                                </div> */}
+                                <div className="form-group">
+                                    <input type="email" ref={emailRef} className="form-input" name="email" id="email" placeholder="Your Email" />
                                 </div>
                                 <div className="form-group">
-                                    <input type="email" className="form-input" name="email" id="email" placeholder="Your Email" />
-                                </div>
-                                <div className="form-group">
-                                    <input type="text" className="form-input" name="password" id="password" placeholder="Password" />
+                                    <input type="text"  ref={passwordRef} className="form-input" name="password" id="password" placeholder="Password" />
                                     <span toggle="#password" className="zmdi zmdi-eye field-icon toggle-password" />
                                 </div>
                                 <div className="form-group">
-                                    <input type="password" className="form-input" name="re_password" id="re_password" placeholder="Repeat your password" />
+                                    <input type="password" ref={passwordConfirmRef} className="form-input" name="re_password" id="re_password" placeholder="Repeat your password" />
                                 </div>
                                 <div className="form-group">
                                     <input type="checkbox" name="agree-term" id="agree-term" className="agree-term" />
