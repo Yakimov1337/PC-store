98bb810ba5b522269e5fad13ea49f0ec6188119b
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-24 05:51:39 +0200

    Create Marketplace.js

---
+++ src/components/Marketplace/Marketplace.js
@@ -0,0 +1,18 @@
+export default function Marketplace(){
+    require('./style.css');
+    
+      return (
+  
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://www.corsair.com/corsairmedia/sys_master/productcontent/CH-9300011-NA-M65_PRO_RGB_BLK_04.png" alt="mouse corsair" className="mouse" />
+          </div>
+          <div className="contentBox">
+            <h3>Mouse Corsair M65</h3>
+            <h2 className="price">61.<small>98</small> €</h2>
+            <a href="#" className="buy">Buy Now</a>
+          </div>
+          
+        </div>
+      ); 
+  }
