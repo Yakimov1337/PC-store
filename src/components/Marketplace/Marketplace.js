e2df6d8d8de48bd3eb98cff9e9c225a3bd7184d7
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-28 23:23:48 +0200

    Keys fix

--- src/components/Marketplace/Marketplace.js
+++ src/components/Marketplace/Marketplace.js
@@ -1,34 +1,42 @@
 import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard.js";
-import { collection, getDocs } from "@firebase/firestore";
-import { db } from '../../firebase'
+import { getFirestore, collection, getDoc, getDocs } from "@firebase/firestore";
+
+
+
 
 export default function Marketplace() {
     require('./style.css');
-
+    const db = getFirestore();
+    
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
-
+    
     useEffect(async () => {
         setLoading(true);
         const querySnapshot = await getDocs(collection(db, "products"));
         const items = [];
         querySnapshot.forEach((doc) => {
-            items.push(doc.data());
+
+            let product = {
+                id: doc.id,
+                ...doc.data(),
+            };
+
+            items.push(product);
+            // console.log(`${doc.id}`);
         });
         setProducts(items);
         setLoading(false);
 
     }, [])
 
-
     if (loading) return <h1> Loading </h1>
 
     return (
         <section id="catalog-page">
-
-            {products
-                ? products.map(product => <ProductCard key={product._id} product={product} />)
+            {products.length > 0 && !loading
+                ? products.map(product => <ProductCard key={product.id} product={product} />)
                 : <h3 className="no-articles">There are no available products right now!</h3>
             }
         </section>
