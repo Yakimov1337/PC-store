40f4138d07e42f47fd761e0c4cc2ce0539759949
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-28 06:43:57 +0200

    Label create

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -54,13 +54,16 @@ export default function Register() {
                                     <input type="text" className="form-input" name="name" id="name" placeholder="Your Name" />
                                 </div> */}
                                 <div className="form-group">
+                                <label class="label" for="email">Email</label>
                                     <input type="email" ref={emailRef} className="form-input" name="email" id="email" placeholder="Your Email" />
                                 </div>
                                 <div className="form-group">
+                                <label class="label" for="password">Password</label>
                                     <input type="password"  ref={passwordRef} className="form-input" name="password" id="password" placeholder="Password" />
                                     <span toggle="#password" className="zmdi zmdi-eye field-icon toggle-password" />
                                 </div>
                                 <div className="form-group">
+                                <label class="label" for="password">Repeat password</label>
                                     <input type="password" ref={passwordConfirmRef} className="form-input" name="re_password" id="re_password" placeholder="Repeat your password" />
                                 </div>
                                 <div className="form-group">
