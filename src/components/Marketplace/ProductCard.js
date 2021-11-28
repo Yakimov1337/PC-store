385fbf6c7ede3d4398159b671cb8f7ae3af6f287
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-28 23:24:00 +0200

    List keys fix

--- src/components/Marketplace/ProductCard.js
+++ src/components/Marketplace/ProductCard.js
@@ -4,7 +4,6 @@ export default function Product({
     product
 }) {
     return (
-        <div>
             <div className="product-card">
                 <div className="imgBox">
                     <img src="https://www.corsair.com/corsairmedia/sys_master/productcontent/CH-9300011-NA-M65_PRO_RGB_BLK_04.png" alt="mouse corsair" className="mouse" />
@@ -13,10 +12,8 @@ export default function Product({
 
                     <h3>{product.name}</h3>
                     <h2 className="price">{product.price} €</h2>
-                   <Link to={`/details/${product._id}`} className="buy">Details</Link>
+                   <Link to={`/product/${product.id}`} className="buy">Details</Link>
                 </div>
-
             </div>
-        </div>
     )
 }
--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,8 +1,8 @@
 export default function Preloader() {
     return [
         
-            <div id="preloader"></div>,
-            <a href="#" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
+            <div id="preloader" key="1jkh213jk1h23jkh12jk3"></div>,
+            <a key="1jkh213jk1h23jkh12jk3222" href="#" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
         
     ];
 };
