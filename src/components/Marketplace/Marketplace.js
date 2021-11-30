0812838e8d4d216b5de595da2a9a6ad74bedac13
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-30 05:55:53 +0200

    Import fix

--- src/components/Marketplace/Marketplace.js
+++ src/components/Marketplace/Marketplace.js
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard.js";
 import { getFirestore, collection, getDoc, getDocs } from "@firebase/firestore";
-
+import { db } from "../../firebase.js";
 
 
 
 export default function Marketplace() {
     require('./style.css');
-    const db = getFirestore();
     
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -17,19 +16,26 @@ export default function Marketplace() {
         const querySnapshot = await getDocs(collection(db, "products"));
         const items = [];
         querySnapshot.forEach((doc) => {
-
             let product = {
                 id: doc.id,
                 ...doc.data(),
             };
-
             items.push(product);
-            // console.log(`${doc.id}`);
         });
         setProducts(items);
         setLoading(false);
-
     }, [])
+
+    //WORKS BUT TAKES 1S TO RENDER ALL PRODUCTS IDK WHY
+    // useEffect(async()=> {
+    //     setLoading(true);
+    //     const getAllProducts = async () =>{
+    //         const data = await getDocs(collection(db,"products"));
+    //       setProducts(data.docs.map((doc) => ({...doc.data(),id:doc.id})));
+    //     };
+    //     getAllProducts();
+    //     setLoading(false);
+    // },[]);
 
     if (loading) return <h1> Loading </h1>
 
