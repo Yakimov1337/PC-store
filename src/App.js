7df69d382d6c3a1c0fcd0077bb35f5dc040abb34
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-12-07 07:29:57 +0200

    Import routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Switch } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext.js';
 import Footer from './components/Footer/Footer.js';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword.js';
 import Header from './components/NavBar/NavBar';
@@ -8,7 +9,6 @@ import Marketplace from './components/Marketplace/Marketplace.js';
 import MyProfile from './components/MyProfile/MyProfile.js';
 import Preloader from './components/Preloader/Preloader';
 import Register from './components/Register/Register.js';
-import { AuthProvider } from './contexts/AuthContext.js';
 import PrivateRoute from '../src/contexts/PrivateRoute.js';
 import About from './components/About/About.js';
 import AddProduct from './components/AddProduct/Add.Product.js';
@@ -18,7 +18,20 @@ import Services from './components/Services-comp/Services.js';
 import Team from './components/Team/Team.js';
 import ProductDetails from './components/Details/ProductDetails.js';
 import MyProducts from './components/MyProducts/MyProducts.js';
-import GPUs from './components/Marketplace/GPUs.js';
+import GPUs from './components/Marketplace/GPU/GPUs.js';
+import NVIDIA from './components/Marketplace/GPU/NVIDIA';
+import AMD from './components/Marketplace/GPU/AMD';
+import ASUS from './components/Marketplace/Motherboard/ASUS';
+import ASRock from './components/Marketplace/Motherboard/ASRock';
+import MOTHERBOARDS from './components/Marketplace/Motherboard/All';
+import MSI from './components/Marketplace/Motherboard/MSI';
+import CPU from './components/Marketplace/CPU/All';
+import PSU from './components/Marketplace/PSU/All';
+
+
+
+
+
 
 
 
@@ -39,10 +52,18 @@ function App() {
           <Route path='/team' component={Team} />
           <Route path='/contact-us' component={ContactUs} />
           <Route path='/marketplace' component={Marketplace} />
-          <Route exact path='/marketplaceall-gpus' component={GPUs} />
-          <Route path='/product/:productId' component={ProductDetails} />
+          <Route exact path='/marketplace-all-gpus' component={GPUs} />
+          <Route exact path='/marketplace-all-motherboards' component={MOTHERBOARDS} />
+          <Route exact path='/marketplace-all-cpu' component={CPU} />
+          <Route exact path='/marketplace-all-psu' component={PSU} />
+          <Route exact path='/marketplace-nvidia' component={NVIDIA} />
+          <Route exact path='/marketplace-amd' component={AMD} />
+          <Route exact path='/marketplace-asrock' component={ASRock} />
+          <Route exact path='/marketplace-msi' component={MSI} />
+          <Route exact path='/marketplace-asus' component={ASUS} />
+          <Route path='/product-:productId' component={ProductDetails} />
           <Route path='/add-product' component={AddProduct} />
-          <Route path='/edit/:productId' component={EditProduct} />
+          <Route path='/edit-:productId' component={EditProduct} />
           <PrivateRoute path='/my-profile' component={MyProfile} />
           <PrivateRoute path='/my-products' component={MyProducts} />
         </Switch>
