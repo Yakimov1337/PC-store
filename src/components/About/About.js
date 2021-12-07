6e5df56ef728c7ae9451e6da657fcea88744feef
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-12-07 18:53:01 +0200

    About page final

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,33 +2,46 @@ export default function About() {
     require('./about.style.css');
     return (
         <div>
-               <section id="about-page" className="about-page">
-                    <div className="container" data-aos="fade-up">
-
-                        <div className="row">
-                            <div className="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-                                <img src="assets/img/about.jpg" className="img-fluid" alt="" />
-                            </div>
-                            <div className="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
-                                <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-                                <p className="fst-italic">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                    magna aliqua.
-                                </p>
-                                <ul>
-                                    <li><i className="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                                    <li><i className="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                                    <li><i className="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-                                </ul>
-                                <p>
-                                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-                                </p>
-                            </div>
-                        </div>
-
+            <section id="about-title" className="about-title">
+                <div className="container" data-aos="fade-up">
+                    <div className="section-title">
+                        <h2>About</h2>
+                        <p>Why we are here</p>
                     </div>
-                </section>
+                </div>
+            </section>
+
+            <section id="about" className="about">
+                <div className="container" data-aos="fade-up">
+
+                    <div className="row">
+                        <div className="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+                            <img src="assets/img/photo.jpg" className="img-fluid" alt="" />
+                        </div>
+                        <div className="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+                            <h3>Welcome to PC-STORE, your number one source for digital products. We're dedicated to giving you the very best of digital world, with a focus on best marketplace on the web.</h3>
+                            <p className="fst-italic">
+                                Founded in 1996 by George Yakimov, PC-STORE has come a long way from its beginnings in Bulgaria.
+                                When Geore first started out, his passion for leading digital marketplace
+                                drove him to do tons of researches how to satisfy every user needs so that PC-STORE can offer you
+                                the world most advanced store. We now serve customers all over the world,
+                                and are thrilled that we're able to turn our passion into our own website.Since then PC-STORE expanded its services and now
+                                offers not only great user experience while searching for digital product but also:
+                            </p>
+                            <ul>
+                                <li><i className="ri-check-double-line"></i> Computer Service & Repair.</li>
+                                <li><i className="ri-check-double-line"></i> Cloud Services.</li>
+                                <li><i className="ri-check-double-line"></i> Security & Identity</li>
+                            </ul>
+                            <p>
+                            We hope you enjoy our products as much as we enjoy offering them to you.
+                             If you have any questions or comments, please don't hesitate to contact us.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
         </div>
     );
 };
