d8c07e714502bad30353f5cf42d46b1fc5a34188
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-12-07 18:53:11 +0200

    Create NotFound.js

---
+++ src/components/404-NotFound/NotFound.js
@@ -0,0 +1,15 @@
+export default function NotFound() {
+    require('./notfound.style.css')
+    return (
+        <section id="breadcrumbs-not-found" className="breadcrumbs-not-found">
+        <div className="container-not-found">
+            <div className="section-title-not-found">
+                <h2>PAGE NOT FOUND</h2>
+            </div>
+        </div>
+
+    </section>
+     
+
+    );
+};
